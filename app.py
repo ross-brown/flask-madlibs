cedf849f9d1601a53d0395d80db9c46c9c907f61
Nathan Berger <nberger101@gmail.com>
2023-08-23 20:15:42 -0400

    got through to code review no errors

--- app.py
+++ app.py
@@ -11,9 +11,21 @@ debug = DebugToolbarExtension(app)
 
 @app.get("/")
 def show_form():
+    """Show form to input madlib words"""
     input_prompts = silly_story.prompts
 
     return render_template("questions.html", prompts=input_prompts)
+
+@app.get("/results")
+def show_results():
+    """Show results of madlib inputs"""
+    answers = {}
+    for prompt in silly_story.prompts:
+        answers[prompt] = request.args[prompt]
+
+    result = silly_story.get_result_text(answers)
+
+    return render_template("results.html", story_result = result)
 
 
 
